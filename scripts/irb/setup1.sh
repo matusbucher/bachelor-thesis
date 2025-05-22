112074f095d72faf539d1ace7803ef4232ef10ac
matusbucher <bucher.matus@gmail.com>
2025-05-23 00:45:43 +0200

    Finishing thesis

--- scripts/irb/setup1.sh
+++ scripts/irb/setup1.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_DIR=/home/matus/scripts
 
-$SCRIPT_DIR/mount_dev-null.sh
+$SCRIPT_DIR/mount_dev_device.sh null 1 3
+
 $SCRIPT_DIR/copy_command.sh irb
 $SCRIPT_DIR/copy_command.sh ruby3.2
 $SCRIPT_DIR/irb/add_dependencies.sh
--- scripts/perl/setup.sh
+++ scripts/perl/setup.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=/home/matus/scripts
 
-$SCRIPT_DIR/mount_dev-null.sh
+$SCRIPT_DIR/mount_dev_device.sh null 1 3
+
 $SCRIPT_DIR/copy_command.sh perl
 $SCRIPT_DIR/perl/add_dependencies.sh
